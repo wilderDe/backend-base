57a988f1d1dbca9cbec8cfd506a52e2e3e9334e1
WildeDe <quispe.7091360@gmail.com>
2023-07-31 15:11:35 -0400

    backend base nestjs

---
+++ src/application/producto/dto/actualizar-producto.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsNumber } from "src/common/validation"
+
+
+export class ActulizarProductoDto {
+    @IsNotEmpty()
+    nombre: string
+    @IsNumber()
+    cantidad: number
+    @IsNumber()
+    precio: number
+    estado?: string
+}
